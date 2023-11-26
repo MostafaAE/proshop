2767bff464f4f8b37779945b85ece06e9f93e5f7
MostafaAE <mostafa.aymanae@gmail.com>
2023-11-26 22:32:38 +0200

    Implement header component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,17 @@
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+
 function App() {
-  return <h1>Welcome To ProShop</h1>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome To ProShop</h1>
+        </Container>
+      </main>
+    </>
+  );
 }
 
 export default App;
