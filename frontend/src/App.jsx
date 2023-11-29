1368cc787dad01b0802f94ce37a00cc9b6a2cbc0
MostafaAE <mostafa.aymanae@gmail.com>
2023-11-30 01:47:32 +0200

    Implement product details page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,13 @@ import {
 } from 'react-router-dom';
 import AppLayout from './components/AppLayout';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<AppLayout />}>
       <Route index element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
     </Route>
   )
 );
