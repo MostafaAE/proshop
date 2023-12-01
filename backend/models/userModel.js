ed63b642980e95488305ec77b03046a886e2fe05
MostafaAE <mostafa.aymanae@gmail.com>
2023-12-01 22:46:32 +0200

    Implement user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+    default: 'user',
+    enum: ['admin', 'user'],
+  },
+});
+
+const userModel = mongoose.Model('User', userSchema);
+module.exports = userModel;
