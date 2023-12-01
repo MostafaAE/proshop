30dc13aa5f5c76969f984e855a9552e1279efdee
MostafaAE <mostafa.aymanae@gmail.com>
2023-12-01 22:23:52 +0200

    Connect to mongodb database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 const dotenv = require('dotenv');
-
+const connectDB = require('./config/db');
 dotenv.config();
-
 const app = require('./app');
+
+connectDB(); // Connect to MongoDB
 
 // START SERVER
 const port = process.env.PORT || 3000;
