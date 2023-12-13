dbe7ce7a49b1995be22d6b57463c92f4e28e0899
MostafaAE <mostafa.aymanae@gmail.com>
2023-12-14 00:44:24 +0200

    Implement get all products endpoint

---
+++ backend/controllers/productController.js
@@ -0,0 +1,12 @@
+const catchAsync = require('../utils/catchAsync');
+const Product = require('./../models/productModel');
+
+exports.getAllProducts = catchAsync(async (req, res, next) => {
+  const products = await Product.find();
+
+  res.status(200).json({
+    status: 'success',
+    results: products.length,
+    data: products,
+  });
+});
