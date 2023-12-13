951175e099e9d94d7731b99134fbb561c1492993
MostafaAE <mostafa.aymanae@gmail.com>
2023-12-14 00:35:28 +0200

    Implement development logging using morgan

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const products = require('./dev-data/data/products');
+const morgan = require('morgan');
+
 const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
+// Development logging
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
 // Unhandled routes handler middleware
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
