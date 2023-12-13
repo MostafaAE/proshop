d0abd19f6ccc6285b12af7013b93ff5d99f72323
MostafaAE <mostafa.aymanae@gmail.com>
2023-12-14 00:30:26 +0200

    Implement unhandled routes handler middleware

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,10 @@ const products = require('./dev-data/data/products');
 const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
+// Unhandled routes handler middleware
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
 // Error handling middleware
 app.use(globalErrorHandler);
