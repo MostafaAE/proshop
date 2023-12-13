e17c953bc5d09492c103139ea574c592219b6bbe
MostafaAE <mostafa.aymanae@gmail.com>
2023-12-14 00:28:41 +0200

    Implement global error handling middleware

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,10 @@
 const express = require('express');
 const products = require('./dev-data/data/products');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
-app.get('/', (req, res, next) => {
-  res.send('API is running');
-});
-
-app.get('/api/products', (req, res, next) => {
-  res.status(200).json({
-    status: 'success',
-    data: products,
-  });
-});
-
-app.get('/api/products/:id', (req, res, next) => {
-  const { id: productId } = req.params;
-  const product = products.find(p => p._id === productId);
-
-  res.status(200).json({
-    result: 'success',
-    data: product,
-  });
-});
+// Error handling middleware
+app.use(globalErrorHandler);
 
 module.exports = app;
