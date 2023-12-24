07027226c2ccd864c5819d66124d111739b69ff2
MostafaAE <mostafa.aymanae@gmail.com>
2023-12-24 22:04:09 +0200

    Handle JWT errors

--- backend/controllers/errorController.js
+++ backend/controllers/errorController.js
@@ -21,6 +21,12 @@ const handleValidationErrorDB = err => {
   return new AppError(message, 400);
 };
 
+const handleJWTError = () =>
+  new AppError('Invalid token. Please log in again!', 401);
+
+const handleJWTExpiredError = () =>
+  new AppError('Your token has expired! Please log in again.', 401);
+
 const sendErrorDev = (err, req, res) => {
   return res.status(err.statusCode).json({
     status: err.status,
