b5a6996ff1a27c73e3dd763738f16136e60bb241
MostafaAE <mostafa.aymanae@gmail.com>
2023-12-24 21:49:23 +0200

    Implement protect routes middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const userController = require('./../controllers/userController');
+const authController = require('../controllers/authMiddleware');
 
 const router = express.Router();
 
@@ -11,15 +12,16 @@ router.route('/login').post(userController.loginUser);
 router.route('/logout').post(userController.logoutUser);
 
 router.route('/').get(userController.getUsers);
+
+router
+  .route('/profile')
+  .get(authController.protect, userController.getUserProfile)
+  .patch(authController.protect, userController.updateUserProfile);
+
 router
   .route('/:id')
   .get(userController.getUserById)
   .patch(userController.updateUser)
   .delete(userController.deleteUser);
-
-router
-  .route('/profile')
-  .get(userController.getUserProfile)
-  .patch(userController.updateUserProfile);
 
 module.exports = router;
