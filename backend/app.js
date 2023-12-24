847908081d1140c484ea1a3850ba4efcb1708594
MostafaAE <mostafa.aymanae@gmail.com>
2023-12-24 21:48:15 +0200

    Implement user login

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
 
 const globalErrorHandler = require('./controllers/errorController');
 
@@ -12,6 +13,8 @@ if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
 
 // ROUTES
 app.use('/api/products', productRouter);
