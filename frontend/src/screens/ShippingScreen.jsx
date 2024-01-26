393c8ddd83cbac2f4a25543954a9002422ce5eb7
MostafaAE <mostafa.aymanae@gmail.com>
2024-01-26 20:33:03 +0200

    Validate shipping form

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -38,6 +38,7 @@ function ShippingScreen() {
             placeholder="Enter address"
             value={address}
             onChange={e => setAddress(e.target.value)}
+            required
           ></Form.Control>
         </Form.Group>
 
@@ -48,6 +49,7 @@ function ShippingScreen() {
             placeholder="Enter city"
             value={city}
             onChange={e => setCity(e.target.value)}
+            required
           ></Form.Control>
         </Form.Group>
 
@@ -59,6 +61,7 @@ function ShippingScreen() {
             placeholder="Enter postal code"
             value={postalCode}
             onChange={e => setPostalCode(e.target.value)}
+            required
           ></Form.Control>
         </Form.Group>
 
@@ -70,6 +73,7 @@ function ShippingScreen() {
             placeholder="Enter country"
             value={country}
             onChange={e => setCountry(e.target.value)}
+            required
           ></Form.Control>
         </Form.Group>
 
